d4d4176f02a77d7a0b0df9774cfcdaddb9c0d067
UyaSky <m.suryaalghifari@gmail.com>
2025-08-15 00:34:51 +0700

    fix(js-columnsMenu-interface_management): perbaiki bug custom menu colom

--- assets/interface_management/js/grid/columnMenuGuards.js
+++ assets/interface_management/js/grid/columnMenuGuards.js
@@ -3,9 +3,18 @@
 		let colMenuOpen = false;
 		let blockColMenuClose = false;
 		let lastMenuField = null;
+		let $curMenu = null;
 
-		function dbg() {
-			/* optional: console.log('[colmenu]', ...arguments); */
+		const DEBUG = true;
+		const log = (...a) => DEBUG && console.log("[ColMenu]", ...a);
+
+		function armBlock(ms = 500, tag = "") {
+			blockColMenuClose = true;
+			log("BLOCK ON", tag);
+			setTimeout(() => {
+				blockColMenuClose = false;
+				log("BLOCK OFF", tag);
+			}, ms);
 		}
 
 		function applyColumnMenuMaxHeight($ul) {
@@ -24,26 +33,60 @@
 			});
 		}
 
+		function dropdownPopupVisible() {
+			return (
+				$(
+					".jqx-listbox:visible, .jqx-listbox-container:visible, .jqx-popup:visible"
+				).length > 0
+			);
+		}
+
+		function isInsideAllowed(target) {
+			return !!(
+				target.closest &&
+				target.closest(
+					".jqx-popup, .jqx-menu, .jqx-menu-wrapper, .jqx-menu-ul, " +
+						".jqx-dropdownlist, .jqx-listbox, .jqx-listbox-container, .cm-filter-wrap"
+				)
+			);
+		}
+
+		(function attachCaptureSwallow() {
+			const swallow = (ev) => {
+				if (!colMenuOpen) return;
+				if (isInsideAllowed(ev.target)) {
+					ev.stopPropagation();
+					ev.stopImmediatePropagation?.();
+				}
+			};
+			[
+				"pointerdown",
+				"mousedown",
+				"mouseup",
+				"click",
+				"wheel",
+				"touchstart",
+				"touchend",
+			].forEach((t) => document.addEventListener(t, swallow, true));
+		})();
+
 		function wireDropdownGuards($ul) {
-			const $dds = $ul.find(".jqx-dropdownlist");
-			$dds.each(function () {
-				const $dd = $(this);
-				$dd.off(".colmenu");
-				$dd.on("open.colmenu mousedown.colmenu", function () {
-					blockColMenuClose = true;
-				});
-				$dd.on("select.colmenu", function () {
-					blockColMenuClose = true;
-					setTimeout(() => {
-						blockColMenuClose = false;
-					}, 80);
-				});
-				$dd.on("close.colmenu", function () {
-					setTimeout(() => {
-						blockColMenuClose = false;
-					}, 0);
-				});
-			});
+			$ul
+				.find(".jqx-dropdownlist")
+				.off(".colmenu")
+				.on(
+					"open.colmenu pointerdown.colmenu mousedown.colmenu focusin.colmenu",
+					() => armBlock(550, "dd")
+				)
+				.on("select.colmenu close.colmenu", () => armBlock(160, "dd-select"));
+
+			$ul
+				.find(".jqx-listbox, .jqx-listbox-container")
+				.off(".colmenu")
+				.on(
+					"pointerdown.colmenu mousedown.colmenu click.colmenu wheel.colmenu",
+					() => armBlock(550, "lb")
+				);
 		}
 
 		function observeMenu($ul) {
@@ -52,54 +95,53 @@
 			$ul.data("colmenuObs", obs);
 		}
 
-		function dropdownPopupVisible() {
-			return (
-				$(".jqx-listbox:visible, .jqx-listbox-container:visible").length > 0
-			);
-		}
+		function opening(menuEl, datafield) {
+			const $ul = $(menuEl);
+			lastMenuField = datafield || lastMenuField;
+			log("columnmenuopening for", datafield);
 
-		function opening(menu) {
-			const $ul = $(menu);
+			try {
+				$ul.jqxMenu({ autoCloseOnClick: false, autoCloseOnMouseLeave: false });
+
+				$ul
+					.closest(".jqx-menu, .jqx-popup")
+					.jqxMenu?.({ autoCloseOnClick: false, autoCloseOnMouseLeave: false });
+			} catch (e) {
+				/* ignore */
+			}
+
 			applyColumnMenuMaxHeight($ul);
+			wireDropdownGuards($ul);
 		}
 
 		function bindGridMenuHandlers() {
 			const GRID = IM.cfg.GRID;
 
-			// tahan close untuk SEMUA interaksi di panel custom
 			$(document)
 				.off(".colmenu-local")
 				.on(
-					"mousedown.colmenu-local click.colmenu-local",
+					"pointerdown.colmenu-local mousedown.colmenu-local click.colmenu-local keydown.colmenu-local focusin.colmenu-local input.colmenu-local",
 					".cm-filter-wrap, .cm-filter-wrap *",
-					function () {
-						blockColMenuClose = true;
-						setTimeout(() => {
-							blockColMenuClose = false;
-						}, 180);
-					}
+					() => armBlock(550, "local")
 				);
 
-			// tahan close untuk semua popup jqx (listbox/dropdown)
 			$(document)
-				.off(".colmenu-dd-global")
+				.off(".colmenu-dd")
 				.on(
-					"mousedown.colmenu-dd-global click.colmenu-dd-global wheel.colmenu-dd-global",
+					"pointerdown.colmenu-dd mousedown.colmenu-dd click.colmenu-dd wheel.colmenu-dd",
 					".jqx-listbox, .jqx-listbox-container, .jqx-listitem-element, .jqx-popup",
-					function () {
-						blockColMenuClose = true;
-						setTimeout(() => {
-							blockColMenuClose = false;
-						}, 180);
-					}
+					() => armBlock(550, "dd-global")
 				);
 
-			// cegah close ketika masih interaksi di popup
 			$(GRID)
 				.off("columnmenuclosing.im")
 				.on("columnmenuclosing.im", function (e) {
-					if (blockColMenuClose || dropdownPopupVisible()) {
+					const cancel = blockColMenuClose || dropdownPopupVisible();
+					if (cancel) {
 						e.args.cancel = true;
+						log("columnmenuclosing -> CANCEL");
+					} else {
+						log("columnmenuclosing -> allow");
 					}
 				});
 
@@ -109,13 +151,15 @@
 					colMenuOpen = true;
 					lastMenuField = e.args.datafield;
 					const $ul = $(e.args.menu);
+					$curMenu = $ul;
+					log("columnmenuopened", lastMenuField);
 
-					if (!$ul.data("jqxWidget")) {
+					try {
 						$ul.jqxMenu({
 							autoCloseOnClick: false,
 							autoCloseOnMouseLeave: false,
 						});
-					}
+					} catch (e) {}
 					applyColumnMenuMaxHeight($ul);
 					requestAnimationFrame(() => {
 						wireDropdownGuards($ul);
@@ -125,39 +169,13 @@
 
 			$(GRID)
 				.off("columnmenuclosed.im")
-				.on("columnmenuclosed.im", function (e) {
-					const $ul = $(e.args?.menu);
-					$ul?.data("colmenuObs")?.disconnect();
+				.on("columnmenuclosed.im", function () {
+					$curMenu?.data("colmenuObs")?.disconnect();
 					colMenuOpen = false;
 					blockColMenuClose = false;
+					$curMenu = null;
+					log("columnmenuclosed");
 				});
-
-			// guard capture-phase (klik di popup jqx tidak dianggap klik luar)
-			(function attachCaptureGuards() {
-				const isInside = (el) =>
-					el.closest &&
-					el.closest(
-						".jqx-popup, .jqx-menu, .jqx-listbox, .jqx-listbox-container, .jqx-dropdownlist"
-					);
-
-				const swallow = (ev) => {
-					if (!colMenuOpen) return;
-					const inside = isInside(ev.target);
-					if (inside) {
-						ev.stopPropagation();
-						ev.stopImmediatePropagation?.();
-					}
-				};
-				[
-					"pointerdown",
-					"mousedown",
-					"mouseup",
-					"click",
-					"wheel",
-					"touchstart",
-					"touchend",
-				].forEach((t) => document.addEventListener(t, swallow, true));
-			})();
 		}
 
 		return { opening, bindGridMenuHandlers };
